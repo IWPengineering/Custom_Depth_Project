58cb2d110381e4d62af104fa43dc7f2647e36ba0
Paul Zwart <pz1155@messiah.edu>
2016-04-28 11:58:54 -0400

    DisplayCode
    Added libraries for display code, wrote basic code.

---
+++ Code/Display/Display_Code/Display_Code.ino
@@ -0,0 +1,78 @@
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_TFTLCD.h> // Hardware-specific library
+
+// The control pins for the LCD can be assigned to any digital or
+// analog pins...but we'll use the analog pins as this allows us to
+// double up the pins with the touch screen (see the TFT paint example).
+#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
+
+#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
+
+// When using the BREAKOUT BOARD only, use these 8 data lines to the LCD:
+// For the Arduino Uno, Duemilanove, Diecimila, etc.:
+//   D0 connects to digital pin 8  (Notice these are
+//   D1 connects to digital pin 9   NOT in order!)
+//   D2 connects to digital pin 2
+//   D3 connects to digital pin 3
+//   D4 connects to digital pin 4
+//   D5 connects to digital pin 5
+//   D6 connects to digital pin 6
+//   D7 connects to digital pin 7
+// For the Arduino Mega, use digital pins 22 through 29
+// (on the 2-row header at the end of the board).
+
+// Assign human-readable names to some common 16-bit color values:
+#define  BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+// If using the shield, all control and data lines are fixed, and
+// a simpler declaration can optionally be used:
+//Adafruit_TFTLCD tft;
+
+#define tftwidth 320
+#define tftheight 480
+void setup() {
+  Serial.begin(9600);
+
+#ifdef USE_ADAFRUIT_SHIELD_PINOUT
+  Serial.println(F("Using Adafruit 2.8\" TFT Arduino Shield Pinout"));
+#else
+  Serial.println(F("Using Adafruit 2.8\" TFT Breakout Board Pinout"));
+#endif
+  Serial.print("TFT size is "); Serial.print(tft.width()); Serial.print("x"); Serial.println(tft.height());
+
+  tft.reset();
+
+  uint16_t identifier = tft.readID();
+
+    if(identifier == 0x8357) {
+    Serial.println(F("Found HX8357D LCD driver"));
+  } else {
+    Serial.print(F("Unknown LCD driver chip: "));
+    Serial.println(identifier, HEX);
+    /*Serial.println(F("If using the Adafruit 2.8\" TFT Arduino shield, the line:"));
+    Serial.println(F("  #define USE_ADAFRUIT_SHIELD_PINOUT"));
+    Serial.println(F("should appear in the library header (Adafruit_TFT.h)."));
+    Serial.println(F("If using the breakout board, it should NOT be #defined!"));
+    Serial.println(F("Also if using the breakout, double-check that all wiring"));*/
+    return;
+}
+
+tft.begin(identifier);
+tft.fillScreen(BLACK);
+delay(1000);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
--- Libraries/TFTLCD-Library-master/TFTLCD-Library-master/Adafruit_TFTLCD.cpp
+++ Libraries/TFTLCD-Library-master/TFTLCD-Library-master/Adafruit_TFTLCD.cpp
@@ -19,10 +19,10 @@
 #include "pin_magic.h"
 
 #define TFTWIDTH   320
-#define TFTHEIGHT  480//
+#define TFTHEIGHT  480
 
-#define TFTWIDTH   240//
-#define TFTHEIGHT  320
+//#define TFTWIDTH   320
+//#define TFTHEIGHT  480
 
 // LCD controller chip identifiers
 #define ID_932X    0
